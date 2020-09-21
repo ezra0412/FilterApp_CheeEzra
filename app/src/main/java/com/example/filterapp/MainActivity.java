05647c28cc69573d7921c9fd0c4b36b6c22b4141
Ezra <tsuna2002@gmail.com>
2020-09-21 08:42:18 +0800

    Side menu
    Signed-off-by: Ezra <tsuna2002@gmail.com>

--- app/src/main/java/com/example/filterapp/MainActivity.java
+++ app/src/main/java/com/example/filterapp/MainActivity.java
@@ -1,27 +1,51 @@
 package com.example.filterapp;
 
 import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
+import com.example.filterapp.menu.DrawerAdapter;
+import com.example.filterapp.menu.DrawerItem;
+import com.example.filterapp.menu.SimpleItem;
+import com.example.filterapp.menu.SpaceItem;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.yarolegovich.slidingrootnav.SlidingRootNav;
+import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnItemSelectedListener {
     TabLayout tabLayout;
     TabItem generateQR, scanQR;
     ViewPager viewPager;
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
-    Toolbar tb;
+
+    private SlidingRootNav slidingRootNav;
+    private String[] screenTitles;
+    private Drawable[] screenIcons;
+
+    private static final int account = 0;
+    private static final int faq = 1;
+    private static final int logout = 3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +55,37 @@ public class MainActivity extends AppCompatActivity {
         scanQR = findViewById(R.id.tab_item_scanQR_main);
         viewPager = findViewById(R.id.vp_main);
 
-        //tb = findViewById(R.id.tb_main);
-        //  getSupportActionBar(tb);
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.tb_main);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        slidingRootNav = new SlidingRootNavBuilder(this)
+                .withToolbarMenuToggle(toolbar)
+                .withMenuOpened(false)
+                .withContentClickableWhenMenuOpened(true)
+                .withSavedState(savedInstanceState)
+                .withMenuLayout(R.layout.menu_left_drawer)
+                .withDragDistance(150)
+                .withRootViewScale(0.8f)
+                .withRootViewElevation(10)
+                .withRootViewYTranslation(4)
+                .inject();
+
+        screenIcons = loadScreenIcons();
+        screenTitles = loadScreenTitles();
+        SlidingRootNav changeName = slidingRootNav.getLayout();
+        //TextView ChangeName = changeName.findViewById*
+
+        DrawerAdapter adapter = new DrawerAdapter(Arrays.asList(
+                createItemFor(account).setChecked(true),
+                createItemFor(faq),
+                new SpaceItem(48),
+                createItemFor(logout)));
+        adapter.setListener(this);
+
+        RecyclerView list = findViewById(R.id.list);
+        list.setNestedScrollingEnabled(false);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setAdapter(adapter);
 
         MovableFloatingActionButton fab = findViewById(R.id.fab_main);
         CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) fab.getLayoutParams();
@@ -63,16 +116,61 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onItemSelected(int position) {
+        if (position == logout) {
+            logout();
+        } else if (position == account) {
+            Toast.makeText(MainActivity.this, "My account", Toast.LENGTH_LONG).show();
+        }
+        slidingRootNav.closeMenu();
+
+    }
+
+    @SuppressWarnings("rawtypes")
+    private DrawerItem createItemFor(int position) {
+        return new SimpleItem(screenIcons[position], screenTitles[position])
+                .withIconTint(color(R.color.light_green))
+                .withTextTint(color(R.color.light_green))
+                .withSelectedIconTint(color(R.color.light_green))
+                .withSelectedTextTint(color(R.color.light_green));
+    }
+
+
     public void addCustomer(View view) {
         Intent intent = new Intent(MainActivity.this, AddCustomer.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
 
-    public void logout(View view) {
+
+    private String[] loadScreenTitles() {
+        return getResources().getStringArray(R.array.menuScreenTitles);
+    }
+
+    private Drawable[] loadScreenIcons() {
+        TypedArray ta = getResources().obtainTypedArray(R.array.menuScreenIcons);
+        Drawable[] icons = new Drawable[ta.length()];
+        for (int i = 0; i < ta.length(); i++) {
+            int id = ta.getResourceId(i, 0);
+            if (id != 0) {
+                icons[i] = ContextCompat.getDrawable(this, id);
+            }
+        }
+        ta.recycle();
+        return icons;
+    }
+
+
+    public void logout() {
         mAuth.signOut();
         startActivity(new Intent(MainActivity.this, LoginPage.class));
         finish();
+    }
+
+    @ColorInt
+    private int color(@ColorRes int res) {
+        return ContextCompat.getColor(this, res);
     }
 
 }
